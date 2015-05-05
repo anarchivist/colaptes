2f0d64670d6e8261ed8bcf2afea2e196cda4f162
Mark A. Matienzo <mark@matienzo.org>
2015-05-05 16:05:07 -0400

    Add (short) link back to Flickr objects

--- app/controllers/iiif_image_controller.rb
+++ app/controllers/iiif_image_controller.rb
@@ -1,5 +1,9 @@
+require 'base58'
+
 class IiifImageController < ApplicationController
   def show
+    @id = params[:id]
+    @flickr_url = "http://flic.kr/p/#{Base58.encode(@id.to_i)}"
     @iiif_image = Riiif::Engine.routes.url_helpers.info_path(params[:id])
   end
 end
